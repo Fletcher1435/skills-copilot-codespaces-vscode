e33855eadc201833cb86a37cca763f3681f12640
Fletcher1435 <fhohn@g.holycross.edu>
2025-11-16 14:23:20 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,13 @@
+function calculateNumbers(var1, var2) {
+    let sum = var1 + var2;
+    let difference = var1 - var2;
+    let product = var1 * var2;
+    let quotient = var1 / var2;
+
+    return {
+        sum: sum,
+        difference: difference,
+        product: product,
+        quotient: quotient
+    };
+}
